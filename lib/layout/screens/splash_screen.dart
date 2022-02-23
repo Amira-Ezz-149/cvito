bd4092053770c88d41528bbd95281e4c0b4534d9
Amira Ezz <miraezz14@gmail.com>
2022-02-23 02:35:30 +0200

    handling intro_slider

--- lib/layout/screens/splash_screen.dart
+++ lib/layout/screens/splash_screen.dart
@@ -1,18 +1,18 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:cvito/layout/screens/home_screen.dart';
+import 'package:cvito/layout/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
+  static const String id = 'SplashScreen';
 
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-
       backgroundColor: Colors.white,
-      splashIconSize:120.0,
-      splash: Image.network('assets/images/app_logo.png', fit: BoxFit.cover),
-      nextScreen: HomeScreen(),
+      splashIconSize:180.0,
+      splash: Image.asset('assets/images/app_logo.png', fit: BoxFit.cover),
+      nextScreen: const WelcomeScreen(),
       splashTransition: SplashTransition.scaleTransition,
     );
   }
--- lib/layout/widgets/secondary_custom_button.dart
+++ lib/layout/widgets/secondary_custom_button.dart
@@ -1,11 +1,15 @@
 import 'package:cvito/constants.dart';
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget {
-  CustomButton({@required this.text, @required this.function});
+
+/// for company button
+class SecondaryCustomButton extends StatelessWidget {
+  const SecondaryCustomButton({@required this.text, @required this.function,});
 
   final String? text;
   final VoidCallback? function;
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -15,21 +19,15 @@ class CustomButton extends StatelessWidget {
         child: Container(
           alignment: Alignment.center,
           decoration: BoxDecoration(
-              color: kBasicColor,
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.grey,
-                  offset: Offset(0, 3), //(x,y)
-                  blurRadius: 5,
-                ),
-              ],
-              borderRadius: BorderRadius.circular(50.0),),
+            border: Border.all(color: kBasicColor),
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(50.0),),
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 40),
             child: Text(
               text!,
               style: const TextStyle(
-                color: Color(0xffffffff),
+                color: kBasicColor,
                 fontSize: 15,
               ),
             ),
